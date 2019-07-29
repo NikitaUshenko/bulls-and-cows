0912f09e7f7d069a7bbd12a0d0992fef1a9f6a3d
NikitaUshenko <nikita-ushenko@mail.ru>
2019-07-29 12:36:36 +0300

    Updates

---
+++ precache-manifest.2ebf1fd3fa881bcf663bbe8cd7969fd2.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3354dc8b960ec668baedbcbc23e98856",
+    "url": "/bulls-and-cows/index.html"
+  },
+  {
+    "revision": "6a843865a4b8c96c4bd8",
+    "url": "/bulls-and-cows/static/js/main.aeb31b83.chunk.js"
+  },
+  {
+    "revision": "27f6a547cb09a2171556",
+    "url": "/bulls-and-cows/static/js/runtime~main.4298023d.js"
+  }
+]);
